6e856ca6010560a5e75a0cab4e28e59404e3c083
Satinaki <102633354+Satinaki@users.noreply.github.com>
2022-04-26 15:13:28 -0700

    jquery problems

---
+++ project-3/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    //------------------------------------
+    // show hide each itme
+    //------------------------------------
+
+    $('.heading').click(function () {
+        $('.details').show();
+    });
+    //---------------------------
+    // setup show hide all button
+    //---------------------------
+//    $("#showhide").click(function () {
+//
+//        $('.item').toggleClass('active');
+//    });
+//
+//});
